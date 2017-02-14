40d756f21d292d99fefc80211339eb65d49ee6b7
tbrms <mail.dustmail@gmail.com>
2017-02-14 11:06:34 +0100

    cam.sh - Ajout du repertoire de sauvegarde

--- cam/script/cam.sh
+++ cam/script/cam.sh
@@ -1,2 +1,4 @@
 sudo raspistill -vf -hf -w 1000 -h 666 -o /home/xxx/cam.jpg
 sudo cp /home/xxx/cam.jpg /var/www/html/cam.jpg
+sudo cp /home/xxx/cam.jpg /home/xxx/save/cam-`date +%s`.jpg
+sudo /usr/bin/find /home/xxx/save/* -mtime +0 -delete
