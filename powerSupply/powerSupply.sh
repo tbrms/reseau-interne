34425894f180874dfb72db423eac49c16e9f6fe4
tbrms <mail.dustmail@gmail.com>
2020-03-16 14:25:57 +0100

    Update powerSupply.sh

--- powerSupply/powerSupply.sh
+++ powerSupply/powerSupply.sh
@@ -1,1 +1,18 @@
+dateHome=`stat -c %Y ./fileTest`
+now=`date +%s`
+
+diff=$(($now-$dateHome))
+
+delay=1800
+
+echo dateHome: $dateHome
+echo now: $now
+echo diff: $diff
+
+if [ $diff -gt $delay ]
+        then
+                echo "Plus grand"
+        else
+                echo "Plus petit"
+fi
 
